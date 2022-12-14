c6ac96417ab38a3b66807b060248d140ad78a8d6
PythonFX <music_zhm@163.com>
2022-12-14 14:26:14 +0800

    Initial Commit

---
+++ DIDOpen/DIDOpenApp.swift
@@ -0,0 +1,17 @@
+//
+//  DIDOpenApp.swift
+//  DIDOpen
+//
+//  Created by VincentMing on 2022/12/14.
+//
+
+import SwiftUI
+
+@main
+struct DIDOpenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
